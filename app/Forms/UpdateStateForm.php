f1b51d7d47e611bf1ccf6268ebfd975e726dcc8f
alifdelgado <alifdelgado@gmail.com>
2023-08-06 16:41:05 -0600

    States crud added

---
+++ app/Forms/UpdateStateForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Forms;
+
+use App\Models\Country;
+use ProtoneMedia\Splade\AbstractForm;
+use ProtoneMedia\Splade\FormBuilder\Select;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\FormBuilder\Text;
+use ProtoneMedia\Splade\SpladeForm;
+
+class UpdateStateForm extends AbstractForm
+{
+    public function configure(SpladeForm $form)
+    {
+        $form
+            ->method('PATCH')
+            ->class('p-4 space-y-3 bg-white border rounded shadow');
+    }
+
+    public function fields(): array
+    {
+        return [
+            Text::make('name')
+                ->rules(['required', 'max:100', 'min:3'])
+                ->label(__('Name state'))
+                ->placeholder(__('Name state')),
+            Select::make('country_id')
+                ->rules(['required'])
+                ->label('Choose a country')
+                ->placeholder(__('Choose a country'))
+                ->options(Country::pluck('name', 'id')->toArray()),
+            Submit::make()
+                ->label(__('Submit')),
+        ];
+    }
+}
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -10,4 +10,9 @@ class Country extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'code'];
+
+    public function states()
+    {
+        return $this->hasMany(State::class);
+    }
 }
